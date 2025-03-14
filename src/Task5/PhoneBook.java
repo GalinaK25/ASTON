71432c08419866bdd55a0541f2b932fdd0b4863c
Galina K. <gala.kravchenko87@gmail.com>
2025-03-15 00:38:47 +0300

    Task Five (package Task5)

---
+++ src/Task5/PhoneBook.java
@@ -0,0 +1,41 @@
+package Task5;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class PhoneBook {
+    private static Map<String, List<String>> phoneBook;
+
+    public PhoneBook() {
+        phoneBook = new HashMap<>();
+    }
+
+    public void add(String lastName, String phoneNumber) {
+        if (!phoneBook.containsKey(lastName)) {
+            phoneBook.put(lastName, new ArrayList<>());
+        }
+        phoneBook.get(lastName).add(phoneNumber);
+    }
+
+    public static void get(String lastName) {
+        List<String> phones = phoneBook.get(lastName);
+        if (phones == null || phones.isEmpty()) {
+            System.out.println("NO phone number in Phone Book for '" + lastName + "'");
+            return;
+        }
+        System.out.println("Phone numbers for '" + lastName + "': " + phones);
+    }
+
+    public static void main(String[] args) {
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("AAAAA", "11111111");
+        phoneBook.add("CCCCC", "44444444");
+        phoneBook.add("BBBBB", "22222222");
+        phoneBook.add("AAAAA", "33333333");
+        get("AAAAA");
+        get("BBBBB");
+        get("DDDDD");
+    }
+}
